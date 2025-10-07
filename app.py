8f3160af8d44ac4352fea8665cd4016e88ab9ba0
unplugged-aniimesh <sharmaanimesh14@gmail.com>
2025-10-07 20:49:42 +0530

    updated

--- app.py
+++ app.py
@@ -74,6 +74,110 @@ def login():
     return render_template('login.html')
 
 
+
+def get_user_admin(curr_login_id):
+    if 'user_id' in session and curr_login_id==session['user_id']:
+        user=User.query.get(curr_login_id)
+        return user.admin
+    return False
+
+@app.route('/dashboard/<int:curr_login_id>',methods=["GET"])
+def dashboard(curr_login_id):
+    if request.method=="GET":
+        if 'user_id' in session and session['user_id']==curr_login_id:
+            user=User.query.get(curr_login_id)
+            if user.admin:
+                return redirect(url_for('admin_dashboard',curr_login_id=curr_login_id))
+            else:
+                return redirect(url_for('customer_dashboard',curr_login_id=curr_login_id))
+        flash("Please login access to the Dashboard") 
+        return redirect(url_for('logout'))
+    
+@app.route('/admin/<int:curr_login_id>/dashboard',methods=["GET"])
+def admin_dashboard(curr_login_id):
+     if request.method=="GET":
+        if 'user_id' in session and session['user_id']==curr_login_id:
+            user=User.query.get(curr_login_id)
+            if not user.admin:
+                flash('You are not authorized to access this page')
+                return redirect(f'/dashboard/{curr_login_id}')
+            
+            categories=Category.query.all()
+            data={'curr_login_id':curr_login_id,
+                  'categories':categories}
+            
+            return render_template('admin_dashboard.html',data=data,name=User.query.get(curr_login_id).username)
+        
+        return redirect(url_for('logout'))
+
+
+#create_category
+@app.route('/admin/<int:curr_login_id>/create_category',methods=["GET","POST"])
+def create_category(curr_login_id):
+    if not get_user_admin(curr_login_id):
+        flash('You are not authorized to access this page')
+        return redirect(url_for('logout'))
+    
+    if request.method=="POST":
+        name=request.form['name']
+        
+        
+        try:
+            category=Category(name=name)
+            db.session.add(category)
+            db.session.commit()
+            return redirect(url_for('admin_dashboard',curr_login_id=curr_login_id))
+       
+        except IntegrityError:
+            db.session.rollback()
+            flash('Category is already there')
+            return redirect(url_for('create_category',curr_login_id=curr_login_id))
+        
+    return render_template('create_category.html',curr_login_id=curr_login_id)    
+        
+
+@app.route('/admin/<int:curr_login_id>/edit_category/<int:category_id>',methods=["GET","POST"])
+def edit_category(curr_login_id,category_id):
+    if not get_user_admin(curr_login_id):
+        flash('You are not authorized to access this page')
+        return redirect(url_for('logout'))
+    
+    
+    category=Category.query.get_or_404(category_id)
+    
+    if request.method=="POST":   
+        try:
+            category.name=request.form['name']
+            db.session.commit()
+            return redirect(url_for('admin_dashboard',curr_login_id=curr_login_id,category_id=category.id))
+       
+        except IntegrityError:
+            db.session.rollback()
+            flash('Category is already there')
+            return redirect(url_for('create_category',curr_login_id=curr_login_id,category_id=category.id))
+        
+    return render_template('edit_category.html',curr_login_id=curr_login_id,category=category)       
+    
+            
+@app.route('/admin/<int:curr_login_id>/remove/<int:category_id>', methods=['GET', 'POST'])
+def remove_category(curr_login_id, category_id):
+    if not get_user_admin(curr_login_id):
+        flash('You are not authorized to access this page.')
+        return redirect(url_for('logout'))
+
+    category = Category.query.get_or_404(category_id)
+
+    if request.method == 'POST':
+        db.session.delete(category)
+        db.session.commit()
+        return redirect(url_for('admin_dashboard', curr_login_id=curr_login_id))
+
+    return render_template('remove_category.html', curr_login_id=curr_login_id, category=category)
+
+
+
+
+
 @app.route('/logout')
 def logout():
     session.clear()
